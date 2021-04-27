eca164800ee2ed907e054034666b9d19f98eee36
lusongsong <740660230@qq.com>
2021-04-27 20:43:08 +0800

    调整配置

--- common/src/test/java/com/fox/stockhelpercommon/ExampleUnitTest.java
+++ common/src/test/java/com/fox/stockhelpercommon/ExampleUnitTest.java
@@ -1,5 +1,10 @@
 package com.fox.stockhelpercommon;
 
+import com.fox.spider.stock.constant.StockConst;
+import com.fox.spider.stock.entity.vo.StockVo;
+import com.fox.stockhelpercommon.entity.stock.po.StockMinuteKLinePo;
+import com.fox.stockhelpercommon.spider.out.StockSpiderRealtimeMinuteKLineApi;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,5 +18,15 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void spiderTest() {
+        StockSpiderRealtimeMinuteKLineApi stockSpiderRealtimeMinuteKLineApi =
+                new StockSpiderRealtimeMinuteKLineApi();
+        StockMinuteKLinePo stockMinuteKLinePo =
+                stockSpiderRealtimeMinuteKLineApi
+                        .realtimeMinuteKLine(new StockVo("603383", StockConst.SM_A));
+        System.out.println(stockMinuteKLinePo);
     }
 }
